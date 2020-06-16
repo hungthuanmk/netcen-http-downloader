a9bb23a492a7f976926d6d95bd65e3982226c2e9
Khoa Tran <tranduckhoatcu@gmail.com>
2020-06-17 00:10:11 +0700

    finish in web_extractor.py

--- web_extractor.py
+++ web_extractor.py
@@ -10,6 +10,9 @@ import requests
 import mimetypes
 import re
 
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
+                         'AppleWebKit/537.36 (KHTML, like Gecko) '\
+                         'Chrome/75.0.3770.80 Safari/537.36'}
 
 def getDomain(url):    
         parts = re.split("\/", url)
@@ -23,7 +26,7 @@ def getDomain(url):
 
 def mediafire(url):
     try:
-        response = requests.get(url, allow_redirects=False)
+        response = requests.get(url,headers=headers, allow_redirects=False)
         if(response.status_code == 404):
             print("Invalid Mediafire URL \n")
         else:
@@ -49,8 +52,10 @@ def google(url):
             if match:
                 id = match.group(1)
                 break
+
+        # url = FILE_URL.format(id=id, confirm='')
         response = requests.get(
-            url, headers={'Cookie': '', 'User-Agent': 'Mozilla/5.0'})
+            url, headers=headers)
         if(response.status_code == 404):
             print("Invalid Google Drive URL \n")
         else:
@@ -62,12 +67,8 @@ def google(url):
 
 def dropbox(url):
     try:
-        response = requests.get(url.replace("?dl=0", "?dl=1"), headers={
-                                'Cookie': '', 'User-Agent': 'Mozilla/5.0'})
-        if(response.status_code == 404):
-            print("Invalid Dropbox URL \n")
-        else:
-            return response.url
+        response = requests.get(url.replace("?dl=0", "?dl=1"), headers=headers)
+        return response.url
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
 
@@ -75,9 +76,37 @@ def dropbox(url):
 def github(url):
     try:
         response = requests.head(
-            url, headers={'Cookie': '', 'User-Agent': 'Mozilla/5.0'})
+            url, headers=headers)
         if(response.status_code == 404):
-            print("Invalid Github URL \n")
+            return None
+        else:
+            return url+"/archive/master.zip"
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def youtube(url):
+    try:
+        with requests.Session() as session:
+            response = session.post("https://keepvid.works/?url="+url+"#dlURL")
+            soup = BeautifulSoup(session.get(
+                response.url,headers=headers).text, features='lxml')
+            x = soup.findAll("a", {"class": "btn btn-lg btn-danger"})
+            if (x != []):
+                for a in x:
+                    return (a['download'],a['href'])
+            else:
+                return None
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
+def github(url):
+    try:
+        response = requests.head(
+            url, headers=headers)
+        if(response.status_code == 404):
+            return None
         else:
             return url+"/archive/master.zip"
     except requests.exceptions.HTTPError as err:
@@ -90,15 +119,16 @@ def youtube(url):
         with requests.Session() as session:
             response = session.post("https://keepvid.works/?url="+url+"#dlURL")
             soup = BeautifulSoup(session.get(
-                response.url).text, features='lxml')
+                response.url,headers=headers).text, features='lxml')
             x = soup.findAll("a", {"class": "btn btn-lg btn-danger"})
             if (x != []):
                 for a in x:
                     return (a['download'],a['href'])
             else:
-                print("Invalid Youtube URL \n")
+                return None
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
+
 
 
 def facebook(url):
@@ -106,20 +136,20 @@ def facebook(url):
         with requests.Session() as session:
             response = session.post("https://keepvid.works/?url="+url+"#dlURL")
             soup = BeautifulSoup(session.get(
-                response.url).text, features='lxml')
+                response.url,headers=headers).text, features='lxml')
             x = soup.findAll("a", {"class": "btn btn-lg btn-danger"})
             if (x != []):
                 for a in x:
                     return a['href']
             else:
-                print("Invalid Youtube URL \n")
+                return None
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
 
 def get_file_name(url):
     web_domain = getDomain(url)
     if(web_domain == 'googleusercontent'):
-        response = requests.get(url.split("?e=download")[0].split("/")[-1])
+        response = requests.get(url.split("?e=download")[0].split("/")[-1],headers=headers)
         if(response.status_code == 404):
             print("Invalid URL \n")
         else:
@@ -133,9 +163,15 @@ def get_file_name(url):
             print("Invalid URL \n")
         else:
             return response.headers['Content-Disposition'].split("'")[-1]
+    elif(web_domain == 'google'):
+        response = requests.head(url)
+        if(response.status_code == 404):
+            print("Invalid URL \n")
+        else:
+            return data['Content-Disposition'].split(";")[1].split('"')[1]
     else:
         return re.search(r'(?<=\/)[^\/\?#]+(?=[^\/]*$)', url).group(0)
-
+ 
 def redirect_url(url):
     web_domain = getDomain(url)
     if(web_domain == 'github'):
@@ -143,7 +179,6 @@ def redirect_url(url):
     else:
         with requests.Session() as session:
             return session.head(session.post(url).url)
-
 
 domain_crawler_mapper = {
     "mediafire": mediafire,
@@ -154,22 +189,36 @@ domain_crawler_mapper = {
     "facebook": facebook
 }
 
-
-def guess_type_of(link):
-    link_type, _ = mimetypes.guess_type(link)
-    if link_type is None:
-        link_type = requests.head(link).headers
-    if('Content-Type' in link_type):  # True if this url is text/html, False if is a file
-        web_domain = getDomain(link)
-        if web_domain in domain_crawler_mapper:
-            return domain_crawler_mapper[web_domain](link)
-        else:
-            return "Not supported this domain."
+def direct_link_generator(url):
+    response = redirect_url(url)
+    url = response.url
+    if(response.status_code == 404 or response.status_code == 403 or response.status_code == 400):
+        return "Invalid url"
     else:
-        return link
+        url_type, _ = mimetypes.guess_type(url)
+        if url_type is None:
+                 url_type = response.headers
+        if(('Content-Type' in url_type) and (url_type['Content-Type'] == 'text/html; charset=utf-8')):  # True if this url is text/html, False if is a file
+            web_domain = getDomain(url)
+            if web_domain in domain_crawler_mapper:
+                direct_url = domain_crawler_mapper[web_domain](url)
+                if(direct_url):
+                    if(type(direct_url) is not tuple):
+                        return (get_file_name(direct_url),direct_url)
+                    else:
+                        return direct_url
+                else:
+                    return "Invalid url"
+            elif (web_domain == 'googlevideo' or 'googleusercontent' or 'dropboxusercontent'):
+                return (get_file_name(url),url)
+            else:
+                return "Not supported this domain."
+        else:
+            return (get_file_name(url),url)
+
 
 
 if __name__ == "__main__":
-    data = guess_type_of(
+    data = direct_link_generator(
         'https://www.facebook.com/rgb.vn/videos/1396783103844096/')
     print(data)
